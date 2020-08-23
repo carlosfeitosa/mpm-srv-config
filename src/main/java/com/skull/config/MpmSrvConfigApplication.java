931dbaa9ed89f86a8da13ac11351b701c077f6ee
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-08-23 16:07:52 -0300

    commit inicial

---
+++ src/main/java/com/skull/config/MpmSrvConfigApplication.java
@@ -0,0 +1,13 @@
+package com.skull.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmSrvConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmSrvConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skull/config/MpmSrvConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skull.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmSrvConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
