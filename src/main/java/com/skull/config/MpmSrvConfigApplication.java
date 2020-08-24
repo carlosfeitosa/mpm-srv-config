6a430927462df49074563abf179496025010d886
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-08-24 01:08:59 -0300

    configuração inicial

--- src/main/java/com/skull/config/MpmSrvConfigApplication.java
+++ src/main/java/com/skull/config/MpmSrvConfigApplication.java
@@ -2,8 +2,17 @@ package com.skull.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Main class for config server
+ * 
+ * @author Carlos Feitosa (carlos.feitosa.nt@gmail.com)
+ * @since 2020-08-23
+ *
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class MpmSrvConfigApplication {
 
 	public static void main(String[] args) {
